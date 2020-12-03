0cbc845fd96c0a4982256c28b925c759fc3a3349
jlm <j.l.messenger@ieee.org>
2020-12-03 14:31:09 +0000

    Initial commit

---
+++ oct_api.rb
@@ -0,0 +1,119 @@
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+####
+
+require 'rubygems'
+require 'rest-client'
+require 'json'
+
+class OctAPI
+  attr_reader :postcode
+  attr_accessor :logger
+
+  def initialize(key, logger = nil)
+    @key = key
+    @logger = logger
+    if $DEBUG
+      @api = RestClient::Resource.new('https://api.octopus.energy/v1/', user: @key, password: '', verify_ssl: OpenSSL::SSL::VERIFY_NONE)
+    else
+      @api = RestClient::Resource.new('https://api.octopus.energy/v1/', user: @key, password: '')
+    end
+  end
+
+  def octofetch(spec, params = {})
+    begin
+      option_hash = {:accept => :json }
+      option_hash[:params] = params if params
+      resp = @api[spec].get option_hash
+    rescue => e
+      abort("octopus-api: #{spec}: " + e.message)
+    end
+    JSON.parse(resp)
+  end
+
+  ######
+  # Set the postcode, and derive the grid supply point list from it.
+  ######
+  def postcode=(postcode)
+    @postcode = postcode
+    gsps = postcode && gsps(postcode)
+    if gsps['count'] == 1
+      @pes_name = gsps['results'].first['group_id']
+      @logger.info("postcode <#{postcode}>: grid supply point PES name: #{@pes_name}") if @logger
+    else
+      @logger.warn("postcode <#{postcode}>: grid supply point not uniquely found") if @logger
+    end
+  end
+
+  def gsps(postcode)
+    params = { postcode: postcode }
+    octofetch('industry/grid-supply-points/', params)
+  end
+
+  def emps(mpan)
+    octofetch("electricity-meter-points/%s" % mpan)
+  end
+
+  def consumption(mpan, serial, page_size = 48, period_from = nil, period_to = nil, group_by = nil)
+    params = { :page_size => page_size.to_s, :order_by => 'period' }
+    params[:period_from] = period_from if period_from
+    params[:period_to] = period_to if period_to
+    octofetch("electricity-meter-points/#{mpan}/meters/#{serial}/consumption/", params)
+  end
+
+  def products(params = {})
+    result = []
+    begin
+      response = octofetch("products/", params)
+      result += response['results']
+      params[:page] = response['next'] && response['next'].split('page=')[1]
+    end while params[:page]
+    result
+  end
+
+  def tariff_charges(prodcode, tariffcode, params={})
+    sc =  octofetch("products/#{prodcode}/electricity-tariffs/#{tariffcode}/standing-charges/", params)
+    sur = octofetch("products/#{prodcode}/electricity-tariffs/#{tariffcode}/standard-unit-rates/", params)
+    { sc: sc, sur: sur}
+  end
+
+  ######
+  # Retrieve details of a product.  I wanted this method to return an object containing the details, but I don't
+  # know how to do this properly.  Currently, it's implemented by deriving a class from OctAPI and creating an instance
+  # of it to hold the data.  My worry is that this creates too much overhead, including a new RestClient instance for
+  # each product query.
+  ######
+  def product(code)
+    OctAPI::Product.new(@key, code)
+  end
+end
+
+class OctAPI::Product < OctAPI
+  attr_reader :tariffs_active_at, :is_variable, :available_from, :available_to, :is_business, :is_green, :is_prepay,
+              :is_restricted, :is_tracker, :full_name, :display_name, :term, :brand
+
+  def initialize(key, code)
+    super(key)
+    @code = code
+    prod = octofetch("products/#{code}/")
+    @tariffs_active_at = Time.parse(prod['tariffs_active_at']).getlocal(0)
+    @full_name = prod['full_name']
+    @display_name = prod['display_name']
+    @description = prod['description']
+    @is_variable = prod['is_variable']
+    @is_green = prod['is_green']
+    @is_tracker = prod['is_tracker']
+    @is_prepay = prod['is_prepay']
+    @is_business = prod['is_business']
+    @is_restricted = prod['is_restricted']
+    @term = prod['term']
+    @available_from = prod['available_from'] && Time.parse(prod['available_from']).getlocal(0)
+    @available_to = prod['available_to'] && Time.parse(prod['available_to']).getlocal(0)
+    @brand = prod['brand']
+    @is_variable = prod['is_variable']
+  end
+end
---
+++ octoload.rb
@@ -0,0 +1,158 @@
+#!/usr/bin/env ruby
+####
+# Copyright 2020-2020 John Messenger
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+####
+
+require 'rubygems'
+require 'slop'
+require 'open-uri'
+require 'date'
+require 'json'
+require 'rest-client'
+require 'yaml'
+require 'nokogiri'
+require 'logger'
+require 'csv'
+
+require './oct_api'
+
+class Time
+  def bod
+    arr = self.to_a
+    arr[0] = 0
+    arr[1] = 0
+    arr[2] = 0
+    Time.local *arr
+  end
+end
+
+begin
+  opts = Slop.parse do |o|
+    o.string '-s', '--secrets', 'secrets YAML file name', default: 'secrets.yml'
+    o.bool   '-d', '--debug', 'debug mode'
+    o.bool   '-p', '--slackpost', 'post alerts to Slack for new items'
+    o.string '--postcode', 'installation postcode'
+    o.string '--emps', 'list electricity meter points for a given MPAN'
+    o.bool   '-c', '--consumption', 'fetch consumption data in some format'
+    o.bool   '-a', '--all', 'don\'t stop at first already-existing item'
+    o.string '-f', '--from', 'start at this datetime'
+    o.string '-t', '--to', 'stop at this datetime'
+    o.string '--at', 'select products available at a particular date'
+    o.string '--csv', 'write output to this file in CSV format'
+    o.bool   '--products', 'retrieve product information from the Octopus API'
+    o.string '-m', '--match', 'select products matching the given string in their display name'
+    o.string '--product', 'retrieve details of a single product'
+    o.on '--help' do
+      STDERR.puts o
+      exit
+    end
+  end
+  config = YAML.load(File.read(opts[:secrets]))
+
+  # Set up logging
+  $DEBUG = opts.debug?
+  $logger = Logger.new(STDERR)
+  $logger.level = Logger::INFO
+  $logger.level = Logger::DEBUG if $DEBUG
+  #
+  # Log in to the 802.1 Maintenance Database
+  #
+  if $DEBUG
+    RestClient.proxy = "http://localhost:8888"
+    $logger.debug("Using HTTP proxy #{RestClient.proxy}")
+  end
+
+  #
+  # If we are posting to Slack, open the Slack webhook
+  #
+  if opts[:slackpost]
+    slack = RestClient::Resource.new(config['slack_webhook'])
+  end
+
+  from = opts[:from] ? Time.parse(opts[:from]).iso8601 : nil
+  to = opts[:to] ? Time.parse(opts[:to]).iso8601 : nil
+  at = opts[:at] ? Time.parse(opts[:at]).iso8601 : nil
+
+  $logger.debug("from: #{from.to_s}; to: #{to.to_s}; at: #{at.to_s}")
+
+  octo = OctAPI.new(config['key'], $logger)
+  octo.postcode = opts[:postcode]
+
+  #####
+  #####     ELECTRICITY METER POINTS
+  #####
+  if opts[:emps]
+    emps = octo.emps(opts[:emps] + '/')
+    if emps
+      puts "mpan: #{emps['mpan']}: GSP: #{emps['gsp']}, profile class: #{emps['profile_class']}"
+    end
+  end
+
+  #####
+  #####     PRODUCTS
+  #####
+  if opts[:products]
+    params = at ? { available_at: at } : nil
+    products = octo.products(params)
+    products.select! { |p| p['display_name'].match(Regexp.new(opts[:match])) } if opts[:match]
+    products.each do |product|
+      puts "#{product['code']}: #{product['display_name']}"
+
+    end
+    twit = 36
+  end
+
+  if opts[:product]
+    p = octo.product(opts[:product])
+    puts "Product #{opts[:product]} tariffs active at #{p.tariffs_active_at}"
+  end
+
+  #####
+  #####     CONSUMPTION
+  #####
+  if opts[:consumption]
+    if opts[:csv]
+      csv = CSV.open(opts[:csv], 'wb')
+    else
+      csv = nil
+    end
+
+    cons = octo.consumption(config['mpan'], config['serial'], 25000, from, to)
+    twit = 35
+    results = cons['results']
+    ######
+    # Output the consumption data to a CSV file, if selected
+    # The header row says "Date," and then 00:00,00:30,01:00,01:30 etc.
+    ######
+    row = ['Date']
+    48.times do |col|
+      row << Time.parse(results[col]['interval_start']).getlocal(0).strftime('%H:%M')
+    end
+    csv << row if csv
+    ######
+    # Each line in the table starts with YYYY-MM-DD and then real numbers giving consumption in kWh per slot
+    ######
+    (0..(results.length / 48 - 1)).each { |rowno|
+      row = []
+      row << Time.parse(results[48 * rowno]['interval_start']).getlocal(0).strftime("%Y-%m-%d")
+      48.times do |col|
+        row << results[48 * rowno + col]['consumption'].to_f
+      end
+      csv << row if csv
+    }
+
+    csv.close if csv
+  end
+end
