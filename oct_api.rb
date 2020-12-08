1f40900d9748e2ebe2ed6d7bbd0b05dddcffc3e2
jlm <j.l.messenger@ieee.org>
2020-12-08 23:30:03 +0000

    Remove use of global DEBUG variable.  Pass options instead. Make private methods private and move to end of file. Suppress some Rubocop warnings. Add some documentation in YARD for selected methods.

--- oct_api.rb
+++ oct_api.rb
@@ -14,39 +14,17 @@ class OctAPI
   attr_reader :postcode
   attr_accessor :logger
 
-  def initialize(key, logger = nil)
+  # @param [String] key Octopus API key
+  # @param [Logger] logger A previously-created Logger object
+  # @param [Hash] options Use this to pass `verify_ssl: OpenSSL::SSL::VERIFY_NONE`
+  # @return [OctAPI] A newly-created OctAPI instance
+  def initialize(key, logger = nil, options = {})
     @key = key
     @logger = logger
-    if $DEBUG
-      @api = RestClient::Resource.new('https://api.octopus.energy/v1/', user: @key, password: '', verify_ssl: OpenSSL::SSL::VERIFY_NONE)
-    else
-      @api = RestClient::Resource.new('https://api.octopus.energy/v1/', user: @key, password: '')
-    end
+    @api = RestClient::Resource.new('https://api.octopus.energy/v1/', options.merge({ user: @key, password: '', }))
   end
 
-  def octofetch(spec, params = {})
-    begin
-      option_hash = {:accept => :json }
-      option_hash[:params] = params if params
-      resp = @api[spec].get option_hash
-    rescue => e
-      abort("octopus-api: #{spec}: " + e.message)
-    end
-    JSON.parse(resp)
-  end
-
-  def octofetch_array(spec, params = {})
-    result = []
-    myparams = params.dup
-    begin
-      response = octofetch(spec, myparams)
-      result += response['results']
-      myparams[:page] = response['next'] && response['next'].split('page=')[1]
-    end while myparams[:page]
-    result
-  end
-
-  ######
+   ######
   # Set the postcode, and derive the grid supply point list from it.
   ######
   def postcode=(postcode)
@@ -54,22 +32,25 @@ class OctAPI
     gsps = postcode && gsps(postcode)
     if gsps && (gsps['count'] == 1)
       @pes_name = gsps['results'].first['group_id']
+      #noinspection RubyNilAnalysis
       @logger.info("postcode <#{postcode}>: grid supply point PES name: #{@pes_name}") if @logger
     else
       @logger.warn("postcode <#{postcode}>: grid supply point not uniquely found") if @logger
     end
   end
 
+  #noinspection SpellCheckingInspection
   def gsps(postcode)
     params = { postcode: postcode }
     octofetch('industry/grid-supply-points/', params)
   end
 
+  #noinspection SpellCheckingInspection
   def emps(mpan)
     octofetch("electricity-meter-points/%s" % mpan)
   end
 
-  def consumption(mpan, serial, page_size = 48, period_from = nil, period_to = nil, group_by = nil)
+  def consumption(mpan, serial, page_size = 48, period_from = nil, period_to = nil, _group_by = nil)
     params = { :page_size => page_size.to_s, :order_by => 'period' }
     params[:period_from] = period_from if period_from
     params[:period_to] = period_to if period_to
@@ -99,8 +80,10 @@ class OctAPI
     when :sr_gas
       sc =  octofetch_array("products/#{prodcode}/gas-tariffs/#{tariffcode}/standing-charges/", params)
       rates = octofetch_array("products/#{prodcode}/gas-tariffs/#{tariffcode}/standard-unit-rates/", params)
+    else
+      raise ArgumentError, 'tarriftype must be :sr_elec, :dr_elec or :sr_gas'
     end
-    return sc, rates
+    [sc, rates]
   end
 
   Product = Struct.new(:tariffs_active_at, :is_variable, :available_from, :available_to, :is_business, :is_green, :is_prepay,
@@ -109,28 +92,6 @@ class OctAPI
   TariffSummary = Struct.new(:tariff_code, :tariff_type, :payment_model, :sc_excvat, :sc_incvat, :sur_excvat, :sur_incvat,
                              :dur_excvat, :dur_incvat, :nur_excvat, :nur_incvat,
                              keyword_init: true)
-
-  def make_array_of_tariffs(pmg, tariff_type)
-    # tariff_type is :sr_elec, :dr_elec or :sr_gas
-    tariff_list = []
-    pmg.each do |payment_model, tariff_summary|
-      ts = TariffSummary.new(
-        tariff_code: tariff_summary['code'],
-        tariff_type: tariff_type,
-        payment_model: payment_model,
-        sc_excvat: tariff_summary['standing_charge_exc_vat'],
-        sc_incvat: tariff_summary['standing_charge_inc_vat'],
-        sur_excvat: tariff_summary['standard_unit_rate_exc_vat'],
-        sur_incvat: tariff_summary['standard_unit_rate_inc_vat'],
-        dur_excvat: tariff_summary['day_unit_rate_exc_vat'],
-        dur_incvat: tariff_summary['day_unit_rate_inc_vat'],
-        nur_excvat: tariff_summary['night_unit_rate_exc_vat'],
-        nur_incvat: tariff_summary['night_unit_rate_inc_vat']
-      )
-      tariff_list << ts
-    end
-    tariff_list
-  end
 
   ######
   # Retrieve details of a product.
@@ -170,4 +131,57 @@ class OctAPI
     end
     result
   end
+
+  private
+
+  def octofetch(spec, params = {})
+    begin
+      option_hash = {:accept => :json }
+      option_hash[:params] = params if params
+      resp = @api[spec].get option_hash
+    rescue => e
+      abort("octopus-api: #{spec}: " + e.message)
+    end
+    JSON.parse(resp)
+  end
+
+  def octofetch_array(spec, params = {})
+    result = []
+    myparams = params.dup
+    begin
+      response = octofetch(spec, myparams)
+      result += response['results']
+      myparams[:page] = response['next'] && response['next'].split('page=')[1]
+    end while myparams[:page]
+    result
+  end
+
+  # Translate a payment method group returned from the Octopus API into an array of TariffSummary objects.
+  # The payment method and tariff type are squashed into the TariffSummary objects.
+  #
+  # @param [Hash<String, Hash>] pmg A hash whose keys are payment method names (e.g., 'direct_debit_monthly')
+  #     and whose values are hashes representing tariff summaries.
+  # @param [:sr_elec,:dr_elec,:sr_gas] tariff_type The tariff type.  (see TARIFF_TYPES)
+  # @return [Array<TariffSummary>] An array of struct TariffSummary objects
+  def make_array_of_tariffs(pmg, tariff_type)
+    tariff_list = []
+    pmg.each do |payment_model, tariff_summary|
+      ts = TariffSummary.new(
+        tariff_code: tariff_summary['code'],
+        tariff_type: tariff_type,
+        payment_model: payment_model,
+        sc_excvat: tariff_summary['standing_charge_exc_vat'],
+        sc_incvat: tariff_summary['standing_charge_inc_vat'],
+        sur_excvat: tariff_summary['standard_unit_rate_exc_vat'],
+        sur_incvat: tariff_summary['standard_unit_rate_inc_vat'],
+        dur_excvat: tariff_summary['day_unit_rate_exc_vat'],
+        dur_incvat: tariff_summary['day_unit_rate_inc_vat'],
+        nur_excvat: tariff_summary['night_unit_rate_exc_vat'],
+        nur_incvat: tariff_summary['night_unit_rate_inc_vat']
+      )
+      tariff_list << ts
+    end
+    tariff_list
+  end
+
 end
